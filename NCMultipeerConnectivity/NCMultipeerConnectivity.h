916a9615c50d8cbec341b76eecb1459ac0903c7a
Chengzhao Li <leslie_lcz@hotmail.com>
2016-06-30 11:12:07 -0600

    Initial Commit

---
+++ NCMultipeerConnectivity/NCMultipeerConnectivity.h
@@ -0,0 +1,13 @@
+//
+//  NCMultipeerConnectivity.h
+//  NCMultipeerConnectivity
+//
+//  Created by Chengzhao Li on 2016-06-30.
+//  Copyright © 2016 Chengzhao Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCMultipeerConnectivity : NSObject
+
+@end
